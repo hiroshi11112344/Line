b2812ae03b7d5ce82dda4051aa7b38098b064d71
hiroshi11112344 <hikikaeru0303@icloud.com>
2025-03-01 13:24:21 +0900

    userコントローラー、プロフィール新規作成と更新を同時に行えれるよう修正

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,16 +8,16 @@ class UserController < ApplicationController
     
   end
 
-  def create
-    @profile = Profile.new(
-      user_id: current_user.id,  # に紐付ける
-      profile_image: params[:profile][:profile_image],
-      has_partner: false, #パートナー（恋人）がいるかどうか
-      unique_id: SecureRandom.hex(4), #ランダムで生成された一意の識別ID
-      friend_requests: [], #友達申請
-      friends: [] # 友達リスト
-    )
-  
+  def update_or_create
+
+    @profile = current_user.profile || Profile.new(user_id: current_user.id)
+
+    @profile.assign_attributes(
+      has_partner: params[:profile][:has_partner] || false,
+      unique_id: @profile.unique_id || SecureRandom.hex(4),
+      friend_requests: params[:profile][:friend_requests] || [],
+      friends: params[:profile][:friends] || []
+    )   
     # 🔹 生年月日を `birthdate` に変換してセット
     birth_year = params.dig(:profile, :birth_year).to_i
     birth_month = params.dig(:profile, :birth_month).to_i
@@ -28,7 +28,10 @@ class UserController < ApplicationController
     end
   
     # 🔹 LINEの `current_user.image` を `profile_image` に保存（`profile_image` が空なら）
-    if @profile.profile_image.blank? && current_user.image.present?
+
+    if params[:profile][:profile_image].present?
+      @profile.profile_image.attach(params[:profile][:profile_image])
+    elsif @profile.profile_image.blank? && current_user.image.present?
       downloaded_image = URI.open(current_user.image)
       @profile.profile_image.attach(io: downloaded_image, filename: "line_profile.jpg", content_type: "image/jpeg")
     end
